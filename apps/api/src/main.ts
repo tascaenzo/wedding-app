83569453aa582ccf8438150ed72e1f6e9cfa2923
enzo <enzo@iMac-Pro-enzo.homenet.telecomitalia.it>
2022-05-28 02:08:46 +0200

    https config api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,12 +5,26 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  let httpsOptions = {};
+
+  if (process.env.NODE_ENV === 'production') {
+    httpsOptions = {
+      key: fs.readFileSync(process.env.NX_CERT_KEY),
+      cert: fs.readFileSync(process.env.NX_CERT_KEY),
+      ca: fs.readFileSync(process.env.NX_CERT_CA),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    ...httpsOptions,
+    cors: true,
+  });
   const port = process.env.NX_PORT || 3333;
+
   app.enableCors({
     //origin: ['http://localhost:8080', 'http://192.168.1.70:8080'],
     origin: '*',
@@ -20,9 +34,7 @@ async function bootstrap() {
   });
 
   await app.listen(port, '0.0.0.0');
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
