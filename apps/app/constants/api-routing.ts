a5c6ab9e145006b2c363f1adb1ed194c1f52eef4
enzo <enzo@Mac-mini-di-enzo.local>
2025-06-01 12:32:34 +0200

    hot fix url api

--- apps/app/constants/api-routing.ts
+++ apps/app/constants/api-routing.ts
@@ -1,5 +1,5 @@
 //export const API_URL = process.env.NX_API_HOST;
-export const API_URL = 'http://api.marcoeaurora.it';
+export const API_URL = 'https://api.marcoeaurora.it';
 
 export const GET_USER = '/users';
 export const CREATE_USER = '/users';
